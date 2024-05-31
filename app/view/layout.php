f4b5c5ff38fa513340b9bbaf79131fe5d342cb1b
Medrhz <sirhziza371@gmail.com>
2024-05-31 02:32:16 +0100

    change placement of assets folder

--- app/view/layout.php
+++ app/view/layout.php
@@ -4,12 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="./app/assets/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <title><?= $title ?></title>
 </head>
 
 <body>
-    ff
     <div class="container my-5">
         <?= $content ?>
     </div>
